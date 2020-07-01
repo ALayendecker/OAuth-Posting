891381d6e761c8db14ac6ac0f12d6773c81fa870
Alayendecker <alcommits@gmail.com>
2020-07-01 09:23:40 -0500

    added handlebars

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
@@ -9,6 +10,19 @@ dotenv.config({ path: "./config/config.env" });
 connectDB();
 
 const app = express();
+
+//logging with morgan
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//Handlebars
+app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
 
 const PORT = process.env.PORT || 3001;
 
